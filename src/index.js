0735d24c8e536543e9ca15cd50bdab98f7d40323
Katia Romaniuk <katia-l@mail.ru>
2018-02-24 12:55:00 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let modufyBrackets = bracketsConfig.map(e => e.join(''));
+    let resultString = str;
+    for (let i=0; i<str.length/2;i++) {
+        let arr = [...modufyBrackets].filter(e => resultString.includes(e));
+        let position = resultString.indexOf(arr.length ? arr[0] : null);
+        if (position < 0) {
+            break;
+        } else {
+            resultString = resultString.slice(0,position) + resultString.slice(position+2);
+        }
+    }
+    return !resultString.length;
+};
